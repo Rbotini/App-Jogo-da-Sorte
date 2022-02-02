f52fc7273b2fa1f988d09c8cc95e18f5b4b32374
renato botini <botinirenato@gmail.com>
2022-02-02 09:23:41 -0300

    inserido a função sorteio ao projeto

--- app/src/main/java/com/example/jogodasorte/MainActivity.java
+++ app/src/main/java/com/example/jogodasorte/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.jogodasorte;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "Sorteio";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortear(View view){
+        EditText numero = findViewById(R.id.editTextNumero);
+        TextView jogar = findViewById(R.id.textViewNumeroSorteado);
+        int x = new Random().nextInt(10);
+        Log.i(TAG, "sortear:  "+ x);
+        int numeroJogador = Integer.parseInt(numero.getText().toString()) ;
+        if (numeroJogador == x){
+            jogar.setText("Você acertou numero Sorteio:"+x);
+        }else{
+            jogar.setText("Você errou numero Sorteio:"+x);
+        }
+
+
     }
 }
